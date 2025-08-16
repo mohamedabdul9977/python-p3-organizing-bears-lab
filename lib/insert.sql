498e56fc34986912ae2b982b49bf67a62b2e0332
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-08-16 11:57:06 +0300

    Complete lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,10 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
 VALUES 
-    ('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', TRUE),
-    ('Rowdy', 7, 'F', 'Black', 'Playful', TRUE),
-    ('Tabitha', 3, 'F', 'Yellow', 'Friendly', FALSE),
-    ('Sergeant Brown', 4, 'M', 'Brown', 'Curious', TRUE),
-    ('Melissa', 6, 'F', 'Gray', 'Protective', TRUE),
-    ('Grinch', 6, 'M', 'Gray', 'Protective', FALSE),
-    ('Wendy', 6, 'F', 'Gray', 'Protective', TRUE),
-    (NULL, 6, 'M', 'Gray', 'Protective', TRUE);
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 7, 'F', 'Black', 'Playful', 0),
+    ('Tabitha', 3, 'F', 'Yellow', 'Friendly', 0),
+    ('Sergeant Brown', 4, 'M', 'Brown', 'Curious', 1),
+    ('Melissa', 6, 'F', 'Gray', 'Protective', 1),
+    ('Grinch', 6, 'M', 'Gray', 'Protective', 0),
+    ('Wendy', 6, 'F', 'Gray', 'Protective', 1),
+    (NULL, 6, 'M', 'Gray', 'Protective', 1);
